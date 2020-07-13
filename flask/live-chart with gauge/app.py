64ca67f93995568a9c21f2315843e7b3b6c619e2
Shubham Kondekar <kondekarshubham123@gmail.com>
2020-07-13 17:28:38 +0530

    Flask Charts added

---
+++ flask/live-chart with gauge/app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,url_for,request,redirect, make_response
+import random
+import json
+from time import time
+from random import random
+from flask import Flask, render_template, make_response
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Temperature]
+
+    Temperature = random() * 100
+
+    data = [time() * 1000, Temperature]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
